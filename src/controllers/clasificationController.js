2c0bf58e8d69b0e62602fef4309f73ce36a4efd2
joelyn <yoel.canul.yacg@gmail.com>
2026-09-17 15:32:14 -0500

    fix: mejorando consulta de clasificación para incluir filtrado por nombre de videojuego

--- src/controllers/clasificationController.js
+++ src/controllers/clasificationController.js
@@ -2,20 +2,30 @@ const db = require('../config/database');
 
 const getClasification = async (req, res) => {
     try {
-        const query = `
+        const { juego } = req.query;
+
+        let query = `
             SELECT 
-                u.Gamertag AS JUGADOR,
-                v.Nombre AS VIDEOJUEGO,
-                p.Puntuacion AS PUNTUACIÓN
+                ROW_NUMBER() OVER(ORDER BY p.Puntuacion DESC) AS 'POSICIÓN',
+                j.Gamertag AS 'JUGADOR',
+                v.Nombre AS 'VIDEOJUEGO',
+                p.Puntuacion AS 'PUNTUACIÓN'
             FROM Puntuacion p
-               JOIN Jugador u ON p.fkJugador = u.idJugador
-               JOIN VideoJuego v ON p.fkVideoJuego = v.idVideoJuego
-            ORDER BY p. Puntuacion DESC
+            INNER JOIN Jugador j ON p.fkJugador = j.idJugador
+            INNER JOIN VideoJuego v ON p.fkVideoJuego = v.idVideoJuego
         `;
-        
-        const [clasificacion] = await db.query(query);
-        
-        res.json(clasificacion);
+
+        let queryParams = [];
+
+        if (juego) {
+            query += ` WHERE v.Nombre LIKE ?`;
+            queryParams.push(`%${juego}%`);
+        }
+
+        const [ranking] = await db.query(query, queryParams);
+
+        res.json(ranking);
+
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error al obtener la clasificación' });
